8bc0605b457e060f84ad6e16b34d9a52504a31a1
FinchTong <yizhong.tong@siebresystems.com>
2018-02-07 18:49:12 +0800

    提交代理模式

---
+++ src/com/tong/proxypattern/Client.java
@@ -0,0 +1,18 @@
+package com.tong.proxypattern;
+
+/**
+ * @author finch
+ * @date 2018/2/7
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        IGamePlayer gamePlayer = new GamePlayer("уехЩ");
+        IGamePlayer gamePlayerProxy = new GamePlayerProxy(gamePlayer);
+
+        gamePlayerProxy.login("zhangsan", "123456");
+        gamePlayerProxy.killBoss();
+        gamePlayerProxy.upgrade();
+    }
+
+}
---
+++ src/com/tong/proxypattern/IGamePlayer.java
@@ -0,0 +1,15 @@
+package com.tong.proxypattern;
+
+/**
+ * @author finch
+ * @date 2018/2/7
+ */
+public interface IGamePlayer {
+
+    public void login(String user, String password);
+
+    public void killBoss();
+
+    public void upgrade();
+
+}
